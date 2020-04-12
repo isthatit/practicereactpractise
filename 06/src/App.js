6b0f45f941c237035faf181006ad7035959cfc05
isthatit <isthatitdev@gmail.com>
2020-04-12 22:55:23 +0900

    6장 시작

---
+++ 06/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import IterationSample from "./IterationSample";
+
+function App() {
+  return <IterationSample />;
+}
+
+export default App;
