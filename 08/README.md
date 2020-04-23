35e17e3aab60b330266aeb3d81e6a1217bca27f5
isthatit <isthatitdev@gmail.com>
2020-04-23 17:54:55 +0900

    8장 시작

---
+++ 08/README.md
@@ -0,0 +1,11 @@
+## Hooks
+
+리액트 v16.8에 새로 도입 된 기능. 함수형 컴포넌트에서도 상태관리를 할 수 있는 useState, 랜더링 직후 작업을 설정하는 useEffect 등의 기능을 제공
+
+### useState(초기값)
+
+함수형 컴포넌트에서 상태값을 관리하기 위해 useState 사용
+
+### useEffect
+
+리액트 컴포넌트가 랜더링 될 때마다 특정 작업을 수행하도록 설정할 수 있는 Hook, 클래스형 컴포넌트의 componentDidMount와 componentDidUpdate를 합친 형태로 봐도 됨.
---
+++ 08/src/Info.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+
+const Info = () => {
+  const [name, setName] = useState("");
+  const [nickname, setNickname] = useState("");
+  useEffect(() => {
+    // console.log("랜더링이 완료 되었습니다.");
+    console.log({
+      name,
+      nickname,
+    });
+  });
+
+  // 마운트 될 때만 실행하고 싶을 땐 두번째 파라미터로 빈 배열을 넣어줌
+  useEffect(() => {
+    //console.log("마운트 될 때만 실행");
+  }, []);
+
+  // 특정 값이 업데이트 될 때만 실행
+  useEffect(() => {
+    // console.log("name 값이 업데이트 될 때만 실행");
+  }, [name]);
+
+  // 컴포넌트가 언마운트 될 때나 업데이트 되기 직전에 실행
+  useEffect(() => {
+    return () => {
+      console.log("cleanup");
+    };
+  }, []);
+
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+  const onChangeNickname = (e) => {
+    setNickname(e.target.value);
+  };
+
+  return (
+    <div>
+      <div>
+        <input value={name} onChange={onChangeName} />
+        <input value={nickname} onChange={onChangeNickname} />
+      </div>
+      <div>
+        <div>
+          <b>이름:</b> {name}
+        </div>
+        <div>
+          <b>닉네임:</b> {nickname}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
